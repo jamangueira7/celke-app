a0fc04f8d5e7fad819d366973270a5022270b49e
João Mangueira <jamjs7@gmail.com>
2021-03-05 17:41:13 -0300

    form com configurações basicas

--- src/pages/Orcamento/index.js
+++ src/pages/Orcamento/index.js
@@ -1,15 +1,53 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import {
+    Container,
+    TitleInput,
+    InputForm,
+    BtnSubmitForm,
+    TxtSubmitForm
+} from './styles';
 
 export default function Orcamento () {
     const navigation = useNavigation();
 
     return (
         <Container>
-            <Text>Orçamento</Text>
+            <TitleInput>Nome</TitleInput>
+            <InputForm
+                placeholder="Nome completo"
+                autoCorrect={false}
+            />
+
+            <TitleInput>E-mail</TitleInput>
+            <InputForm
+                placeholder="E-mail"
+                autoCorrect={false}
+            />
+
+            <TitleInput>Phone</TitleInput>
+            <InputForm
+                placeholder="(XX) XXXXX-XXXX"
+                autoCorrect={false}
+            />
+
+            <TitleInput>WhatsApp</TitleInput>
+            <InputForm
+                placeholder="(XX) XXXXX-XXXX"
+                autoCorrect={false}
+            />
+
+            <TitleInput>Projeto</TitleInput>
+            <InputForm
+                placeholder="Fale um pouco do seu projeto"
+                autoCorrect={false}
+            />
+
+            <BtnSubmitForm>
+                <TxtSubmitForm>Cadastrar</TxtSubmitForm>
+            </BtnSubmitForm>
+
         </Container>
     );
 }
