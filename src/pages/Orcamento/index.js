a50e06f3dc8299e637642eb4fd7922ccc05e92ef
João Mangueira <jamjs7@gmail.com>
2021-03-05 19:26:20 -0300

    parte visual concluida e os estados prontos para ser usados

--- src/pages/Orcamento/index.js
+++ src/pages/Orcamento/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
 
 import {
     Container,
@@ -10,44 +10,51 @@ import {
 } from './styles';
 
 export default function Orcamento () {
-    const navigation = useNavigation();
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [whatsApp, setWhatsAppe] = useState('');
+    const [projeto, setProjeto] = useState('');
 
     return (
-        <Container>
-            <TitleInput>Nome</TitleInput>
-            <InputForm
-                placeholder="Nome completo"
-                autoCorrect={false}
-            />
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <Container>
+                <TitleInput>Nome</TitleInput>
+                <InputForm
+                    placeholder="Nome completo"
+                    autoCorrect={false}
+                />
 
-            <TitleInput>E-mail</TitleInput>
-            <InputForm
-                placeholder="E-mail"
-                autoCorrect={false}
-            />
+                <TitleInput>E-mail</TitleInput>
+                <InputForm
+                    placeholder="E-mail"
+                    autoCorrect={false}
+                />
 
-            <TitleInput>Phone</TitleInput>
-            <InputForm
-                placeholder="(XX) XXXXX-XXXX"
-                autoCorrect={false}
-            />
+                <TitleInput>Phone</TitleInput>
+                <InputForm
+                    placeholder="(XX) XXXXX-XXXX"
+                    autoCorrect={false}
+                />
 
-            <TitleInput>WhatsApp</TitleInput>
-            <InputForm
-                placeholder="(XX) XXXXX-XXXX"
-                autoCorrect={false}
-            />
+                <TitleInput>WhatsApp</TitleInput>
+                <InputForm
+                    placeholder="(XX) XXXXX-XXXX"
+                    autoCorrect={false}
+                />
 
-            <TitleInput>Projeto</TitleInput>
-            <InputForm
-                placeholder="Fale um pouco do seu projeto"
-                autoCorrect={false}
-            />
+                <TitleInput>Projeto</TitleInput>
+                <InputForm
+                    placeholder="Fale um pouco do seu projeto"
+                    autoCorrect={false}
+                />
 
-            <BtnSubmitForm>
-                <TxtSubmitForm>Cadastrar</TxtSubmitForm>
-            </BtnSubmitForm>
+                <BtnSubmitForm>
+                    <TxtSubmitForm>Cadastrar</TxtSubmitForm>
+                </BtnSubmitForm>
 
-        </Container>
+            </Container>
+        </ScrollView>
     );
 }
